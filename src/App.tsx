e770d7f9c5fb3bf53aeb6c1f94d0343949215b19
cw <fromnowwon@gmail.com>
2024-02-11 17:27:32 +0900

    Updating & Viewing State

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import React, { useState } from "react";
 import "./App.css";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "./app/store";
 import ReservationCard from "./components/ReservationCard";
+import { addReservation } from "./features/reservationSlice";
 
 function App() {
+	const [reservationNameInput, setReservationNameInput] = useState("");
+
 	const reservations = useSelector(
 		(state: RootState) => state.reservations.value
 	);
+
+	const dispatch = useDispatch();
+
+	const handleAddReservations = () => {
+		if (!reservationNameInput) return;
+		dispatch(addReservation(reservationNameInput));
+	};
 
 	return (
 		<div className="App">
@@ -16,14 +26,19 @@ function App() {
 					<div>
 						<h5 className="reservation-header">Reservations</h5>
 						<div className="reservation-cards-container">
-							{reservations.map((name) => {
-								return <ReservationCard name={name} />;
+							{reservations.map((name, index) => {
+								return (
+									<ReservationCard key={index} name={name} index={index} />
+								);
 							})}
 						</div>
 					</div>
 					<div className="reservation-input-container">
-						<input />
-						<button>Add</button>
+						<input
+							value={reservationNameInput}
+							onChange={(e) => setReservationNameInput(e.target.value)}
+						/>
+						<button onClick={handleAddReservations}>Add</button>
 					</div>
 				</div>
 				<div className="customer-food-container">
--- src/components/ReservationCard.tsx
+++ src/components/ReservationCard.tsx
@@ -1,11 +1,25 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { removeReservation } from "../features/reservationSlice";
 
 interface ReserationCardTypes {
 	name: string;
+	index: number;
 }
 
-const ReservationCard = ({ name }: ReserationCardTypes) => {
-	return <div className="reservation-card-container">{name}</div>;
+const ReservationCard = ({ name, index }: ReserationCardTypes) => {
+	const dispatch = useDispatch();
+
+	return (
+		<div
+			onClick={() => {
+				dispatch(removeReservation(index));
+			}}
+			className="reservation-card-container"
+		>
+			{name}
+		</div>
+	);
 };
 
 export default ReservationCard;
