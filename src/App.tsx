2d7c7449a2c1a5933415763826c2c885ee73ebfc
cw <fromnowwon@gmail.com>
2024-02-11 17:58:00 +0900

    Adding Customer Slice

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "./app/store";
 import ReservationCard from "./components/ReservationCard";
 import { addReservation } from "./features/reservationSlice";
+import CustomerCard from "./components/CustomerCard";
 
 function App() {
 	const [reservationNameInput, setReservationNameInput] = useState("");
@@ -11,6 +12,8 @@ function App() {
 	const reservations = useSelector(
 		(state: RootState) => state.reservations.value
 	);
+
+	const customers = useSelector((state: RootState) => state.customers.value);
 
 	const dispatch = useDispatch();
 
@@ -42,16 +45,15 @@ function App() {
 					</div>
 				</div>
 				<div className="customer-food-container">
-					<div className="customer-food-card-container">
-						<p>Selena Gomez</p>
-						<div className="customer-foods-container">
-							<div className="customer-food"></div>
-							<div className="customer-food-input-container">
-								<input />
-								<button>Add</button>
-							</div>
-						</div>
-					</div>
+					{customers.map((customer) => {
+						return (
+							<CustomerCard
+								id={customer.id}
+								name={customer.name}
+								food={customer.food}
+							/>
+						);
+					})}
 				</div>
 			</div>
 		</div>
--- src/components/ReservationCard.tsx
+++ src/components/ReservationCard.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import { removeReservation } from "../features/reservationSlice";
+import { addCustomer } from "../features/customerSlice";
+import { v4 as uuid } from "uuid";
 
 interface ReserationCardTypes {
 	name: string;
@@ -14,6 +16,13 @@ const ReservationCard = ({ name, index }: ReserationCardTypes) => {
 		<div
 			onClick={() => {
 				dispatch(removeReservation(index));
+				dispatch(
+					addCustomer({
+						id: uuid(),
+						name,
+						food: [],
+					})
+				);
 			}}
 			className="reservation-card-container"
 		>
